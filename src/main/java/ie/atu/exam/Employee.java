b2d29b3b46e145fc4bb77a50ca63793a0f9de5d1
darragh <g00390383>
2022-12-07 11:47:19 +0000

    Declared variables and added default constructor

--- src/main/java/ie/atu/exam/Employee.java
+++ src/main/java/ie/atu/exam/Employee.java
@@ -1,5 +1,21 @@
 package ie.atu.exam;
 
 public class Employee {
+    String name;
+    int ppsn;
+    long phone;
+    String gender;
+    String empType;
+    int age;
+
+    public Employee(String name, int ppsn, long phone, String gender, String empType, int age){
+        this.name = name;
+        this.ppsn = ppsn;
+        this.phone = phone;
+        this.gender = gender;
+        this.empType = empType;
+        this.age = age;
+    }
+
 
 }
