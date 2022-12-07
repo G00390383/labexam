baea1ab0a831da4452005eadea186e3739c839da
darragh <g00390383>
2022-12-07 11:27:23 +0000

    Added employee class and test class for employee as well as the adding all dependencies and plugins to pom file

---
+++ src/test/java/ie/atu/exam/EmployeeTest.java
@@ -0,0 +1,14 @@
+package ie.atu.exam;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
